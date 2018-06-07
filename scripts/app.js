a0c919dc8e5dc846d4012e7bc9a3737acc89c459
Christopher Von Wilczur <cvonwilczur@Christophers-MacBook-Air.local>
2018-06-07 06:59:50 -0700

    minor formatting changes

--- scripts/app.js
+++ scripts/app.js
@@ -1,15 +1,15 @@
-<<<<<<< HEAD
+
 //code for local time (https://codepen.io/deegill/pen/iqJFK)
 $(document).ready(function() {
 // Create two variables with names of months and days of the week in the array
-var monthNames = [ "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" ]; 
+var monthNames = [ "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" ];
 var dayNames= ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
 
 // Create an object newDate()
 var newDate = new Date();
 // Retrieve the current date from the Date object
 newDate.setDate(newDate.getDate());
-// At the output of the day, date, month and year    
+// At the output of the day, date, month and year
 $('#Date').html(dayNames[newDate.getDay()] + " " + monthNames[newDate.getMonth()] + ' ' + newDate.getDate() + ' ' + newDate.getFullYear());
 
 setInterval( function() {
@@ -18,23 +18,23 @@ setInterval( function() {
     // Add a leading zero to the value of seconds
     $("#sec").html(( seconds < 10 ? "0" : "" ) + seconds);
     },1000);
-    
+
 setInterval( function() {
     // Create an object newDate () and extract the minutes of the current time
     var minutes = new Date().getMinutes();
     // Add a leading zero to the minutes
     $("#min").html(( minutes < 10 ? "0" : "" ) + minutes);
     },1000);
-    
+
 setInterval( function() {
     // Create an object newDate () and extract the clock from the current time
     var hours = new Date().getHours();
     // Add a leading zero to the value of hours
     $("#hours").html(( hours < 10 ? "0" : "" ) + hours);
     }, 1000);
-    
-}); 
-=======
+
+});
+
 let list = [];
 
 const addToList = () => {

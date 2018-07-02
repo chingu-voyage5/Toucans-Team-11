2383f0a5e220822ab2affd501186993bc2afdee6
cvonwilczur <cvonwilczur@gmail.com>
2018-07-02 16:09:23 -0700

    consolidated files and unified styling

--- scripts/app.js
+++ scripts/app.js
@@ -1,39 +1,32 @@
+// ========== DATE AND TIME FUNCTIONALITY ==========
 
-//code for local time (https://codepen.io/deegill/pen/iqJFK)
-$(document).ready(function() {
-// Create two variables with names of months and days of the week in the array
-var monthNames = [ "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" ];
-var dayNames= ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
+//  Collecting the clock div from the HTML
+let clock = document.querySelector('.time');
 
-// Create an object newDate()
-var newDate = new Date();
-// Retrieve the current date from the Date object
-newDate.setDate(newDate.getDate());
-// At the output of the day, date, month and year
-$('#Date').html(dayNames[newDate.getDay()] + " " + monthNames[newDate.getMonth()] + ' ' + newDate.getDate() + ' ' + newDate.getFullYear());
+// startTime() gathers the current date and runs checkTime() to update the date
+const startTime = () => {
+  let today = new Date();
+  let hour = today.getHours();
+  let minute = today.getMinutes();
+  let second = today.getSeconds();
 
-setInterval( function() {
-    // Create an object newDate () and extract the second of the current time
-    var seconds = new Date().getSeconds();
-    // Add a leading zero to the value of seconds
-    $("#sec").html(( seconds < 10 ? "0" : "" ) + seconds);
-    },1000);
+  minute = checkTime(minute);
+  second = checkTime(second);
 
-setInterval( function() {
-    // Create an object newDate () and extract the minutes of the current time
-    var minutes = new Date().getMinutes();
-    // Add a leading zero to the minutes
-    $("#min").html(( minutes < 10 ? "0" : "" ) + minutes);
-    },1000);
+  clock.innerHTML = `${hour} : ${minute}`
 
-setInterval( function() {
-    // Create an object newDate () and extract the clock from the current time
-    var hours = new Date().getHours();
-    // Add a leading zero to the value of hours
-    $("#hours").html(( hours < 10 ? "0" : "" ) + hours);
-    }, 1000);
+  let t = setTimeout(startTime, 500);
+}
 
-});
+// checkTime() maintains 0s in front of the date unless it's a double-digit number
+const checkTime = (i) => {
+  if (i < 10) {
+    i = '0' + i
+  };
+  return i;
+}
+
+// ========== TODOLIST FUNCTIONALITY ==========
 
 let list = [];
 const taskInput = document.getElementById('task-input');
@@ -50,8 +43,6 @@ const addToList = () => {
   let listItem = document.createElement('li');
   let trashCanButton = document.createElement('span');
 
-
-
   //Add remove functionality to each trash can span
   trashCanButton.onclick = removeFromList.bind(this, list.length);
 
@@ -62,10 +53,8 @@ const addToList = () => {
   listItem.onclick = function() {
     this.classList.toggle('striked-out');
   }
-
   //Adding new task text to created item
   listItem.innerText = taskValue.value;
-
   //Adding Item to todo
   listItem.insertBefore(trashCanButton, listItem.childNodes[0]);
   todoList.appendChild(listItem);
@@ -81,7 +70,6 @@ const removeFromList = (index) => {
   list.splice(index, 1);
   //clear the DOM list, array of list items will still be there
   todoList.innerHTML = '';
-
   reconstructList();
 }
 
@@ -91,38 +79,114 @@ const reconstructList = () => {
   list.map((task, index) => {
     let listItem = document.createElement('li');
     let trashCanButton = document.createElement('span');
-
-
     //Add remove functionality to each trash can span
     trashCanButton.onclick = removeFromList.bind(this, index);
-
-
     //Add trash can symbol to trashCanButton
     trashCanButton.innerHTML = '<i class="fa fa-times" aria-hidden="true"></i>';
-
-
     //Add strikethrough functionality to each list items
     listItem.onclick = function() {
       this.classList.toggle('striked-out');
     }
-
     //Adding new task text to created item
     listItem.innerText = task;
-
     //Adding Item to todo
     todoList.appendChild(listItem);
-
     //Add trash can to list item
     listItem.insertBefore(trashCanButton, listItem.childNodes[0]);
-
-
   })
-
 }
 
+// ========== BACKGROUND IMAGE FUNCTIONALITY ==========
 
-function getRandomBackgroundImage(min, max) {
-  var index = Math.floor(Math.random() * (max - min)) + min;
-  var url = 'url(' + background_images[index] + ')';
-  return url;
+// function getRandomBackgroundImage(min, max) {
+//   var index = Math.floor(Math.random() * (max - min)) + min;
+//   var url = 'url(' + background_images[index] + ')';
+//   return url;
+// }
+
+// ========== WEATHER APP FUNCTIONALITY ==========
+
+$(document).ready(function() {
+  var fahrenheit, celsius;
+	var weatherApiUrl="https://api.openweathermap.org/data/2.5/weather";
+  var apiKey = "83c792dc1352781699e5826c9c270917";
+	getLatLong();
+	function getWeatherData(){
+		$.ajax({
+			url: weatherApiUrl,
+			type: 'GET',
+			dataType: 'json',
+			success: function(data) {
+				var temperature=data.main.temp;
+				celsius=temperature;
+				fahrenheit=celsius*1.8+32;
+				var icon=data.weather[0].icon;
+				var weatherDetail=data.weather[0].main+", "+data.weather[0].description;
+				$('.weatherDetail').html(weatherDetail);
+				$('.iconpic>img').attr('src','http://openweathermap.org/img/w/'+icon+'.png');
+				$('.temp').html(temperature+"&#8451;");
+			},
+			error: function(err) {
+				alert('Oops something went wrong, Please try again.');
+				console.log(err);
+			}
+		});
+	}
+	function getLatLong(){
+		$.ajax({
+			url: "https://geoip-db.com/json/",
+			type: 'GET',
+      dataType: 'json',
+			success: function(data){
+        var lat = data.latitude;
+        var long = data.longitude;
+        $('.city').html(data.city);
+				$('.country').html(data.country_name);
+        weatherApiUrl += "?lat="+lat+"&lon="+long+"&APPID="+apiKey+"&units=metric";
+        getWeatherData();
+      },
+			error: function(err) {
+				alert('Oops something went wrong, Please try again.');
+				console.log(err);
+			}
+		});
+	}
+	//toggle between celsius and fahrenheit
+	$('.toggle .btn').click(function(){
+		// if the div has id as c then convert temperature to fahrenheit
+		if($('.toggle').attr('id')=='c'){
+			$('.temp').html(fahrenheit+"&#8457;");
+			$('.toggle').attr('id','f');
+		}
+	 else if($('.toggle').attr('id')=='f'){
+		 //else if div as id as f than convert temprature to celsius
+			$('.temp').html(celsius+"&#8451;");
+			$('.toggle').attr('id','c');
+		}
+	});
+});
+
+
+// ========== QUOTE FUNCTIONALITY ==========
+//https://forismatic.com/en/api/
+
+var displayQuote = document.querySelector('.quote');
+
+function populate(text) {
+	console.log(text.quoteText);
+	// FIXME put the text on the page
+  displayQuote.innerHTML = text.quoteText;
 }
+function newQuoteGenerator() {
+  var quoteUrl = "http://api.forismatic.com/api/1.0/?method=getQuote&format=jsonp&lang=en&jsonp=populate"
+
+  var getSettings = {
+  	url: quoteUrl,
+  	dataType: "jsonp",
+  	crossDomain: true
+  };
+
+  $.get(getSettings);
+}
+
+newQuoteGenerator();
